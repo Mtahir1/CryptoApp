96b3ccd63cfed28f001f9ad9a1cde2b17effa967
Tahir Shahzad <tahir.shahzad@pwc.com>
2022-03-21 17:23:18 +0100

    First CryptApp commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+/* var options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/coins',
+    params: {
+      referenceCurrencyUuid: 'yhjMzLPhuIDl',
+      timePeriod: '24h',
+      tiers: '1',
+      orderBy: 'marketCap',
+      orderDirection: 'desc',
+      limit: '50',
+      offset: '0'
+    },
+    headers: {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': '51b8d60a10msha1c54b12978678ap11434djsnb67636fe3f98'
+    }
+  }; */
+
+
